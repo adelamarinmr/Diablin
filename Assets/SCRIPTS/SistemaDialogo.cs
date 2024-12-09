c8adec110f5f83fb2011dac8028014348c726c12
adelamarinmr <adelamarinmr@gmail.com>
2024-12-09 18:06:36 +0100

    dialogos

--- Assets/SCRIPTS/SistemaDialogo.cs
+++ Assets/SCRIPTS/SistemaDialogo.cs
@@ -13,7 +13,10 @@ public class SistemaDialogo : MonoBehaviour
     [SerializeField] private TMP_Text textoDialogo;
 
     private bool escribiendo; //determina si el sist esta escribiendo o no
-    private int IndiceFraseActual;
+    private int IndiceFraseActual;// marca la frase x la q voy
+
+    private DialogoSO dialogoActual; // para almacenar con q dialogo estamos trabajando
+
 
     private void Awake()
     {
@@ -37,28 +40,68 @@ public class SistemaDialogo : MonoBehaviour
 
     public void IniciarDialogo(DialogoSO dialogo)
     {
-        
+        dialogoActual= dialogo;
         marcos.SetActive(true);
+        StartCoroutine(EscribirFrase());
     }
+
+    
+    private IEnumerator EscribirFrase() // q el texto aparezca letra x letra
+    {
+        escribiendo = true;
+
+        textoDialogo.text = ""; //limpio el texto antes de poner una nueva frase
+
+        char[] fraseEnLetras = dialogoActual.frases[IndiceFraseActual].ToCharArray();
+
+        foreach (char letra in fraseEnLetras)
+        {
+            textoDialogo.text += letra;
+            yield return new WaitForSeconds(dialogoActual.tiempoEntreLetras);
+        }
+
+        escribiendo = false;
+    }
+
+
+
+    public void SiguienteFrase()
+    {
+        if (escribiendo) //si estamos escribiendo una frase...
+        {
+            CompletarFrase();
+        }
+
+        else
+        {
+            IndiceFraseActual++;
+            if(IndiceFraseActual < dialogoActual.frases.Length)
+            {
+                StartCoroutine(EscribirFrase());
+            }
+
+            else
+            {
+                TerminarDialogo();
+            }
+        }
+    }
+
+    private void CompletarFrase()
+    {
+        StopAllCoroutines();
+        //pongo la frase de golpe
+        textoDialogo.text= dialogoActual.frases[IndiceFraseActual];
+        escribiendo= false;
+    }
+
 
     private void TerminarDialogo() // q el texto aparezca letra x letra
     {
         marcos.SetActive(false);
-    }
-    
-
-    private void EscribirFrase()
-    {
-        
-    }
-
-    private void SiguienteFrase()
-    {
-
-    }
-
-    private void FFFrase()
-    {
-
+        StopAllCoroutines();
+        IndiceFraseActual = 0;//para posteriores dialogos
+        escribiendo=false;
+        dialogoActual = null;//ya no tenemos ningun dialogo a no ser que me vuelvan a clickar
     }
 }
