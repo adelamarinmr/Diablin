53f3dc810414b45298abed374b99fa1364257a96
adelamarinmr <adelamarinmr@gmail.com>
2024-12-11 21:21:34 +0100

    NPC

---
+++ Assets/SCRIPTS/PlayerVisual.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerVisual : MonoBehaviour
+{
+
+    private Animator anim;
+    [SerializeField] private NavMeshAgent agent;
+
+
+
+    private void Aake()
+    {
+        //Ref a mi animator
+        anim = GetComponent<Animator>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //todos los frames voy actualizando mi velocity en funcion de mi vel actual
+        anim.SetFloat("velocity", agent.velocity.magnitude/agent.speed);
+    }
+}
